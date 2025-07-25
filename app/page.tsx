47a4362761536b3a15a1c90895bccb5a5d608860
Safeya-Yasien <ssafeya5@gmail.com>
2025-07-25 10:33:22 +0300

    add hero section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,51 @@
-export default function Home() {
-  return <div></div>;
+import { Button } from "@/components/ui/button";
+import { stripe } from "@/lib/stripe";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  return (
+    <div>
+      {/* Hero section */}
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          {/* title */}
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to My Ecommerce
+            </h2>
+            <p className="text-neutral-600">
+              Discover the latest products at the best prices.
+            </p>
+            <Button className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white">
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse All Products
+              </Link>
+            </Button>
+          </div>
+
+          {/* image */}
+          <div>
+            <Image
+              src={products.data[1].images[0]}
+              alt="Hero Image"
+              className="rounded"
+              width={450}
+              height={450}
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* carousel */}
+      <section className="py-8"></section>
+    </div>
+  );
 }
