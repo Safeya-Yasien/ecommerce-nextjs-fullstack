9144cd6dfec6eba5c6fc2419791ac92edbd88409
Safeya-Yasien <ssafeya5@gmail.com>
2025-07-27 06:44:32 +0300

    add debounce to products search function

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -3,6 +3,7 @@ import { useMemo, useState } from "react";
 import ProductCard from "./ProductCard";
 import Stripe from "stripe";
 import filterProducts from "@/utils/filterProducts";
+import { useDebounce } from "use-debounce";
 
 interface IProductsListProps {
   products: Stripe.Product[];
@@ -10,19 +11,18 @@ interface IProductsListProps {
 
 const ProductsList = ({ products }: IProductsListProps) => {
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchTermDebounced] = useDebounce(searchTerm, 300);
 
-  const handleSearch = (value: string) => {
+  const onSearchChange = (value: string) => {
     setSearchTerm(value);
   };
 
-  const filteredProducts = useMemo(
-    () =>
-      filterProducts({
-        products,
-        searchTerm,
-      }),
-    [products, searchTerm]
-  );
+  const filteredProducts = useMemo(() => {
+    return filterProducts({
+      products,
+      searchTerm: searchTermDebounced,
+    });
+  }, [products, searchTermDebounced]);
 
   return (
     <>
@@ -35,7 +35,7 @@ const ProductsList = ({ products }: IProductsListProps) => {
           id="search"
           type="search"
           value={searchTerm}
-          onChange={(e) => handleSearch(e.target.value)}
+          onChange={(e) => onSearchChange(e.target.value)}
           placeholder="Search products..."
           aria-label="Search products"
           className="w-full max-w-md px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
