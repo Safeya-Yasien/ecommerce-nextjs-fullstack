6bcb2b2eadaa5961c2fc53820a849ab2a2712ac6
Safeya-Yasien <ssafeya5@gmail.com>
2025-07-28 08:00:50 +0300

    enhance product card style

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -15,24 +15,28 @@ const ProductCard = ({ product }: { product: Stripe.Product }) => {
   const price = ((priceObj.unit_amount ?? 0) / 100).toFixed(2);
 
   return (
-    <Card className="w-full max-w-sm mx-auto flex flex-col h-full">
-      <div className="relative w-full h-60">
+    <Card className="w-full max-w-sm mx-auto flex flex-col h-full gap-4">
+      <div className="relative w-full h-24 ">
         <Image
           src={product.images?.[0] || "/placeholder.png"}
           alt={product.name}
           fill
-          className="object-cover"
+          className="w-full h-full object-cover"
         />
       </div>
 
       <CardHeader>
-        <CardTitle>{product.name}</CardTitle>
+        <CardTitle className="text-base font-semibold line-clamp-2">
+          {product.name}
+        </CardTitle>
         {product.description && (
-          <CardDescription>{product.description}</CardDescription>
+          <CardDescription className="line-clamp-2">
+            {product.description}
+          </CardDescription>
         )}
       </CardHeader>
 
-      <CardContent>
+      <CardContent className="">
         <p className="text-lg font-semibold">${price}</p>
       </CardContent>
 
